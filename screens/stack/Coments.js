1d2502c1bc51a8ac57bc495ec89b9af4d952da88
Fierran <ferransuabc@gmail.com>
2024-05-24 23:02:24 -0700

    pa poder comentar ekizde

--- screens/stack/Coments.js
+++ screens/stack/Coments.js
@@ -7,7 +7,29 @@ const Coments = () => {
     return(
         <View style = {{backgroundColor : "#404040", flex : 1}}>
             <Tweets/>
+            <View style = {{flexDirection: "row", justifyContent: "space-around"}}>
+                    <TextInput
+                        style={styles.input}
+                        color = "white"
+                        placeholder="Comentar"
+                        placeholderTextColor="#999999"
+                    />
+            </View>    
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    input: {
+        height: 40,
+        width: 350,
+        padding: 5,
+        borderColor: "gray",
+        borderWidth: 1,
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        marginBottom: 10,
+        marginTop: "3%",
+      }
+})
 export default Coments;
