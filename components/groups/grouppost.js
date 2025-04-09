9d5be9869c03c1a737dfa9301615f8f2e29cb1d3
Fierran <ferransuabc@gmail.com>
2025-04-08 23:49:57 -0700

    group post render

--- components/groups/grouppost.js
+++ components/groups/grouppost.js
@@ -1,107 +1,85 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { Image, FlatList, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-
-const GroupPost = () => {
-    const [groups, setGroups] = useState([]);
-
-    useEffect(() => {
-        axios.get("https://gameit-d77d4db89096.herokuapp.com/api/post/get_posts")
-            .then((response) => {
-                if (response.data && response.data.posts) {
-                    setGroups(response.data.posts);
-                } else {
-                    console.log("Invalid response structure:", response.data);
-                }
-            })
-            .catch((err) => console.log(err));
-    }, []);
-
-    const renderGroupCard = ({ item }) => {
-        return (
-            <View style={styles.card}>
-                <View style={styles.header}>
-                    <Image style={styles.image} source={{ uri: item.photo_url }} />
-                    <View>
-                        <Text style={styles.name}>{item.user}</Text>
-                        <Text style={styles.username}>{item.username}</Text>
-                    </View>
-                </View>
-                <Image style={styles.post} source={{ uri: item.post.photo_url }} />
-                <View style={styles.footer}>
-                    <TouchableOpacity style={styles.button}>
-                        <Text style={styles.buttonText}>Unirse a Grupo</Text>
-                    </TouchableOpacity>
-                    <Text style={styles.groupInfo}>1 / 4</Text>
-                </View>
-            </View>
-        );
-    };
-
-    return (
-        <FlatList
-            data={groups}
-            keyExtractor={(item) => item.user_id.toString()}
-            renderItem={renderGroupCard}
-            ItemSeparatorComponent={() => <View style={styles.separator} />}
-        />
-    );
-};
-
-const styles = StyleSheet.create({
-    card: {
-        paddingTop: 10,
-        backgroundColor: "#4F4F4F",
-        borderRadius: 10,
-        flex: 1,
-        width: "85%",
-        alignSelf: "center"
-    },
-    header: {
-        flexDirection: "row",
-        padding: 10
-    },
-    image: {
-        width: 48,
-        height: 48,
-        borderRadius: 30
-    },
-    name: {
-        fontSize: 18,
-        color: "white"
-    },
-    username: {
-        color: "gray"
-    },
-    post: {
-        width: 200,
-        height: 200,
-        alignSelf: "center",
-        marginTop: 10
-    },
-    footer: {
-        flexDirection: "row",
-        justifyContent: "space-around",
-        marginVertical: 10
-    },
-    button: {
-        backgroundColor: "#31CE40",
-        borderRadius: 3,
-        width: "40%",
-        height: 30,
-        justifyContent: "center"
-    },
-    buttonText: {
-        color: "white",
-        textAlign: "center"
-    },
-    groupInfo: {
-        color: "white",
-        textAlign: "center"
-    },
-    separator: {
-        height: 20
-    }
-});
-
-export default GroupPost;
+import React from "react";
+ import {Image, FlatList, Alert, View, Text, TextInput, Button, StyleSheet,TouchableOpacity,  TouchableWithoutFeedback} from 'react-native';
+ import Prueba, {Groups} from "../../datos/Prueba";
+ 
+ const GroupPost = () => {
+     return(
+         <FlatList
+         data = {Prueba}
+         ItemSeparatorComponent={() => <Text> </Text>}
+         renderItem={({item : groups}) => (
+             <View key = {groups.id} style = {{paddingTop: 10, backgroundColor: "#4F4F4F", borderRadius: 10, flex : 1, width:"85%", alignSelf:"center"}}>
+                 <View style = {{flexDirection: "row"}}>
+                     <View style = {{padding: 10}}>
+                         <Image style = {styles.image} source={{uri : groups.avatar}}/>
+                     </View>
+                     <View>
+                         <Text style = {styles.name}>{groups.name}</Text>
+                         <Text style = {{color : "gray"}}>{groups.screenName}</Text>
+                     </View>
+                 </View>
+                 <View>
+                     <Image  style = {styles.post} source={{uri : groups.avatar}}/>
+                 </View>
+                 <View style = {{flexDirection: "row", justifyContent: "space-around"}}>
+                     <TouchableOpacity style = {styles.Buttons}>
+                         <Text style = {styles.TextButton}>Unirse a Grupo</Text>
+                     </TouchableOpacity>
+                     <View style>
+                         <Text style = {styles.TextButton}>{groups.players} / 4</Text>
+                     </View>
+                 </View>
+                 <View style={{height: 28}}>
+                     
+                 </View>
+             </View>
+         
+         )}/>
+             
+         
+ 
+     )
+ }
+ 
+ const styles = StyleSheet.create({
+     image : {
+         width: 48,
+         height: 48,
+         borderRadius: 30
+     },
+     name : {
+         fontSize: 18,
+         color: "white"
+     },
+     contentxt : {
+         color : "white",
+         fontWeight: "bold"
+     },
+     text : {
+         color : "white"
+     },
+     post : {
+         width: 200,
+         height: 200,
+         alignSelf: "center",
+         padding: 10
+     },
+     Buttons:{
+         backgroundColor: "#31CE40",
+         borderRadius:3,
+         width: "40%",
+         height: 30,
+         marginTop: "3%",
+     },
+     TextButton:{
+         color: "white",
+         borderColor: "black",
+         textAlign: "center",
+         padding: 10,
+     }
+ 
+ 
+ 
+ })
+ 
+ export default GroupPost
